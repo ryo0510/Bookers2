de2913475743137d5d2180743d05b46073fd67ec
EC2 Default User <ec2-user@ip-172-31-42-65.ec2.internal>
2021-12-11 16:36:23 +0000

    (add)4-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  def create
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:create, :index, :show, :destroy]
 end
